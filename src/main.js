6a5eac87eb416c5a02f3ccd336ac5631b32ddedb
sop <szhan253@ucsc.edu>
2022-04-24 00:41:43 -0700

    Double jump works, sometimes cannot collide with ground

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
+const SCALE = 0.5;
+const tileSize = 35;
+
 let config = {
-    type: Phaser.CANVAS,
+    type: Phaser.WEBGL,
     width: 640,
     height: 320,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
     scene: [Menu,Play,Tutorial]
-}
 
+};
+
+let cursors;
 let game = new Phaser.Game(config);
 
 // set UI sizes
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,11 +8,14 @@ class Play extends Phaser.Scene {
         this.load.path ='./assets/';
         this.load.image ('hell','Hell.png');
         this.load.spritesheet('gOver','GameOver.png',{frameWidth:640,framHeight:320,startFrame:0,endFrame:2});
+        this.load.image('squareKirby', 'squareKirby.png');
+        this.load.image('groundScroll', 'ground.png');
     }
 
     create(){
         // place tile
         this.hell = this.add.tileSprite (0,0,640,320,'hell').setOrigin(0,0);
+        //this.player = new Player(this, game.config.width/2, game.config.height/2, 'squareKirby').setOrigin(0.5, 0);
         // define keys
         keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
@@ -46,10 +49,42 @@ class Play extends Phaser.Scene {
           this.add.text (game.config.width/2, game.config.height/2 + borderPadding *10, 'Press SPACE for Menu', gOverConfig).setOrigin(0.5);
           this.gameOver = true;
         },null,this);
+
+        //Jump Action
+        this.JUMP_VELOCITY = -700;
+        this.SCROLL_SPEED = 4;
+        this.MAX_JUMPS = 2;
+        //this.SCROLL_SPEED = 4;
+        this.physics.world.gravity.y = 2600;
+
+        // make ground tiles group
+        this.ground = this.add.group();
+        for(let i = 0; i < game.config.width; i += tileSize) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'squareKirby', 'block').setScale(SCALE).setOrigin(0);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.ground.add(groundTile);
+        }
+        
+        // put another tile sprite above the ground tiles
+        this.groundScroll = this.add.tileSprite(0, game.config.height-tileSize, game.config.width, tileSize, 'groundScroll').setOrigin(0);
+
+        // set up player
+        this.kirby = this.physics.add.sprite(180, game.config.height/2-tileSize, 'squareKirby', 'side').setScale(SCALE);
+
+        // add physics collider
+        this.physics.add.collider(this.kirby, this.ground); 
+
+        // set up Phaser-provided cursor key input
+        cursors = this.input.keyboard.createCursorKeys();
+
+        
+ 
     }
+    
 
     update(){
-        this.hell.tilePositionX -=4;
+        this.hell.tilePositionX += this.SCROLL_SPEED;
         //game Over restarting choice
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keyR)){
             this.scene.restart();
@@ -57,5 +92,33 @@ class Play extends Phaser.Scene {
         if(this.gameOver && Phaser.Input.Keyboard.JustDown(keySPACE)){
             this.scene.start("menuScene");
         }
+
+        //make ground scroll
+        this.groundScroll.tilePositionX += this.SCROLL_SPEED;
+        // check if alien is grounded
+	    this.kirby.isGrounded = this.kirby.body.touching.down;
+        // if so, we have jumps to spare
+	    if(this.kirby.isGrounded) {
+            //this.alien.anims.play('walk', true);
+	    	this.jumps = this.MAX_JUMPS;
+	    	this.jumping = false;
+	    } else {
+	    	//this.kirby.anims.play('jump');
+	    }
+        // allow steady velocity change up to a certain key down duration
+        // see: https://photonstorm.github.io/phaser3-docs/Phaser.Input.Keyboard.html#.DownDuration__anchor
+	    if(this.jumps > 0 && Phaser.Input.Keyboard.DownDuration(keySPACE, 150)) {
+	        this.kirby.body.velocity.y = this.JUMP_VELOCITY;
+	        this.jumping = true;
+	        //this.upKey.tint = 0xFACADE;
+	    } else {
+	    	//this.upKey.tint = 0xFFFFFF;
+	    }
+        // finally, letting go of the UP key subtracts a jump
+        // see: https://photonstorm.github.io/phaser3-docs/Phaser.Input.Keyboard.html#.UpDuration__anchor
+	    if(this.jumping && Phaser.Input.Keyboard.UpDuration(keySPACE)) {
+	    	this.jumps--;
+	    	this.jumping = false;
+	    }
     }
 }
