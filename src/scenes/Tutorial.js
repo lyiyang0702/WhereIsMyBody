fe4b95cf1212f6c6eafeed90ce1f17772cf39327
Thrisha Kopula <thrishak1@gmail.com>
2022-04-28 11:25:09 -0700

    added cute tutorial page

--- src/scenes/Tutorial.js
+++ src/scenes/Tutorial.js
@@ -3,6 +3,11 @@ class Tutorial extends Phaser.Scene {
         super ("tutorialScene");
     }
 
+    preload() {
+        this.load.path ='./assets/';
+        this.load.image ('tutorial','tutorial.png');
+    }
+
     create(){
         let tempConfig = {
             fontFamily: 'Courier',
@@ -19,12 +24,13 @@ class Tutorial extends Phaser.Scene {
         // define keys
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         // show menu text
-        this.add.text (game.config.width/2, borderPadding, 'Ghost 101',tempConfig);
+        this.tutorial = this.add.tileSprite (0,0,game.config.width,game.config.height,'tutorial').setOrigin(0,0);
+        /* this.add.text (game.config.width/2, borderPadding, 'Ghost 101',tempConfig);
         tempConfig.fontSize = '18px';
         this.add.text (borderPadding, borderPadding*5, '1. Press SPACE to JUMP',tempConfig).setOrigin(0,0);
         this.add.text (borderPadding, borderPadding * 10, '2. Ghost is fragile. Keep away from SALT CIRCLE and FIRE.', tempConfig).setOrigin(0,0);
         this.add.text (borderPadding, borderPadding * 15, '3. If you can\'t protect yourself. YOU HAVE TO START OVER.', tempConfig).setOrigin(0,0);
-        this.add.text (borderPadding, borderPadding * 20, 'GOOD LUCK :) (Press SPACE to START)', tempConfig).setOrigin(0,0);
+        this.add.text (borderPadding, borderPadding * 20, 'GOOD LUCK :) (Press SPACE to START)', tempConfig).setOrigin(0,0); */
     }
     update(){
         if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
