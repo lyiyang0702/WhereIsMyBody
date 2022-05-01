fff032baadf62b9b3ab952a08a9307d79638d2a1
Thrisha Kopula <thrishak1@gmail.com>
2022-04-30 23:57:29 -0700

    added background

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -7,9 +7,34 @@ class Menu extends Phaser.Scene {
     preload(){
         this.load.audio('bgm', './assets/menuSong.wav');
         this.load.audio('select', './assets/selectSound.wav');
+        this.load.spritesheet('enterSprite','./assets/enterAnimation.png',{frameWidth:1280,framHeight:720,startFrame:0,endFrame:3});
+        this.load.spritesheet('titleSprite','./assets/titleAnimation.png',{frameWidth:1280,framHeight:720,startFrame:0,endFrame:3});
+        this.load.spritesheet('tutorialSprite','./assets/tutorialAnimation.png',{frameWidth:1280,framHeight:720,startFrame:0,endFrame:3});
+        this.load.image('bg', './assets/fullBackground.png');
     }
 
     create (){
+        this.anims.create({
+            key:'enterAnimation',
+            frames:this.anims.generateFrameNumbers('enterSprite',{start:0, end: 3, first: 0}),
+            frameRate:10,
+            repeat:-1
+        });
+
+        this.anims.create({
+            key:'titleAnimation',
+            frames:this.anims.generateFrameNumbers('titleSprite',{start:0, end: 3, first: 0}),
+            frameRate:10,
+            repeat:-1
+        });
+
+        this.anims.create({
+            key:'tutorialAnimation',
+            frames:this.anims.generateFrameNumbers('tutorialSprite',{start:0, end: 3, first: 0}),
+            frameRate:10,
+            repeat:-1
+        });
+
         let menuConfig = {
             fontFamily: 'Courier',
             fontSize: '28px',
@@ -23,11 +48,18 @@ class Menu extends Phaser.Scene {
             fixedWidth: 0
         }
 
+
         // show menu text
         this.add.text (game.config.width/2, game.config.height/2 - borderPadding *5, 'WHERE IS MY BODY',menuConfig).setOrigin(0.5);
         this.add.text (game.config.width/2, game.config.height/2 , 'Press SPACE to START', menuConfig).setOrigin(0.5);
         this.add.text (game.config.width/2, game.config.height/2 + borderPadding *5, 'Press ENTER for Ghost 101', menuConfig).setOrigin(0.5);
-        
+        this.bgrd = this.add.tileSprite (0,0,game.config.width,game.config.height,'bg').setOrigin(0,0);
+        this.menuEnter = this.add.sprite(game.config.width/2,game.config.height/2+50,'enterSprite');
+        this.menuEnter.anims.play('enterAnimation');
+        this.menuTitle = this.add.sprite(game.config.width/2,game.config.height/2-5,'titleSprite');
+        this.menuTitle.anims.play('titleAnimation');
+        this.menuTutorial = this.add.sprite(game.config.width/2,game.config.height/2-5,'tutorialSprite');
+        this.menuTutorial.anims.play('tutorialAnimation');
         //define keys
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyENTER = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
@@ -39,7 +71,8 @@ class Menu extends Phaser.Scene {
     }
 
     update(){
-        if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
+        this.bgrd.tilePositionX += 2;
+        if (Phaser.Input.Keyboard.JustDown(keyENTER)) {
             this.scene.start('playScene');   
             let music = this.sound.add('select');
             music.play();
@@ -47,7 +80,7 @@ class Menu extends Phaser.Scene {
         }
 
         // open tutorial
-        if (Phaser.Input.Keyboard.JustDown(keyENTER)){
+        if (Phaser.Input.Keyboard.JustDown(keySPACE)){
             this.scene.start('tutorialScene');
             let music = this.sound.add('select');
             music.play();
